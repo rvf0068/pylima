1552c2711d8e8804f42d6a23e1c1fc4fed8707cc
RosaGranados99 <98921544+RosaGranados99@users.noreply.github.com>
2022-04-28 16:35:58 -0500

    Update __init__.py

--- pylima/__init__.py
+++ pylima/__init__.py
@@ -6,3 +6,20 @@ def my_mul(x, y):
     """A function that multiply. """
     return x*y
 
+from sympy import symbols, Function, Eq, Derivative, dsolve, solve
+
+def sistema(matriz):
+
+    """Dada un lista de listas, regresa un sistema de ecuaciones diferenciales."""
+
+    t = symbols('t')
+
+    x, y = symbols('x y', cls=Function)
+
+    eq1 = Eq(Derivative(x(t), t), matriz[0][0]*x(t) + matriz[0][1]*y(t))
+
+    eq2 = Eq(Derivative(y(t), t), matriz[1][0]*x(t) + matriz[1][1]*y(t))
+
+    sols = dsolve((eq1, eq2))
+
+    return sols[0].rhs, sols[1].rhs
