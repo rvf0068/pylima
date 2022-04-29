ba3c0bb0d74a122223273543e3e773301c35dbd7
YessiRocha <47038042+YessiRocha@users.noreply.github.com>
2022-04-28 20:48:08 -0500

    Add Yessi Function

--- pylima/__init__.py
+++ pylima/__init__.py
@@ -13,21 +13,19 @@ def my_mul(x, y):
     """A function that multiply. """
     return x*y
 
-
+def Yessi():
+    """Función que imprime la frase favorita de Yessi"""
+    #esta función fue definida para probar la forma de cargar una actualización con google colab 
+    print("Toma un tiempo para pensar en se flexible confiar y amar")
+    
+    
 def sistema(matriz):
-
     """Dada un lista de listas, regresa un sistema de ecuaciones diferenciales."""
-
     t = symbols('t')
-
     x, y = symbols('x y', cls=Function)
-
     eq1 = Eq(Derivative(x(t), t), matriz[0][0]*x(t) + matriz[0][1]*y(t))
-
     eq2 = Eq(Derivative(y(t), t), matriz[1][0]*x(t) + matriz[1][1]*y(t))
-
     sols = dsolve((eq1, eq2))
-
     return sols[0].rhs, sols[1].rhs
 
 
