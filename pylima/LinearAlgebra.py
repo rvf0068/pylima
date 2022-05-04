df5e169ed16f345bd5159f2db2a9faf90e4b78d6
VeraniaHdez12 <47038209+VeraniaHdez12@users.noreply.github.com>
2022-05-04 17:17:33 -0500

    New function
    Function that returns powers of a matrix

--- pylima/LinearAlgebra.py
+++ pylima/LinearAlgebra.py
@@ -1,1 +1,11 @@
+import sympy as sp
 
+def potencias_matriz(matriz,n):
+    """Powers of a matrix 
+       Args:
+            parameter1 = matrix of sympy
+            parameter2 = n 
+       Returns:
+            The n powers of a given matrix in a list."""
+    pot_matriz = [matriz**i for i in range(1,n+1)]
+    return pot_matriz
