2429492b176fc08d82b534577eebe314e519a1bc
KarenLechuga <98918000+KarenLechuga@users.noreply.github.com>
2022-05-04 16:55:09 -0500

    Update __init__.py

--- pylima/__init__.py
+++ pylima/__init__.py
@@ -1,9 +1,2 @@
-def my_sum(x, y):
-    """A function that sums. """
-    return x+y
-
-
-def Yessi():
-    """This function print the favorite phrase of Yessi"""
-    print("Toma un tiempo para pensar en ser flexible, confiar y amar")
+from EDO.py import *
     
