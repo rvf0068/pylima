c86d97ae928dcea45ad6eb168e167e2e889a1106
KarenLechuga <98918000+KarenLechuga@users.noreply.github.com>
2022-05-04 16:42:48 -0500

    Agregué función de prueba.

--- pylima/EDO.py
+++ pylima/EDO.py
@@ -1,1 +1,9 @@
-#hola otra vez
+def cuadrados_fibonacci(n):
+    fibs = []
+    a, b = 0, 1
+    while a < n:
+        fibs.append(a)
+        a, b = b, a+b
+    return fibs
+
+cuadrados_fibonacci(100)
