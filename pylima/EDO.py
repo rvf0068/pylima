9b0bb2e214f449b20619aa92e0c54dc19e9c2315
KarenLechuga <98918000+KarenLechuga@users.noreply.github.com>
2022-05-04 17:20:09 -0500

    Update EDO.py

--- pylima/EDO.py
+++ pylima/EDO.py
@@ -1,3 +1,5 @@
+from sympy import symbols, Function, Eq, Derivative, dsolve
+
 def cuadrados_fibonacci(n):
     fibs = []
     a, b = 0, 1
@@ -5,3 +7,13 @@ def cuadrados_fibonacci(n):
         fibs.append(a)
         a, b = b, a+b
     return fibs
+
+
+def sistema(matriz):
+    """Dada un lista de listas, regresa un sistema de ecuaciones diferenciales."""
+    t = symbols('t')
+    x, y = symbols('x y', cls=Function)
+    eq1 = Eq(Derivative(x(t), t), matriz[0][0]*x(t) + matriz[0][1]*y(t))
+    eq2 = Eq(Derivative(y(t), t), matriz[1][0]*x(t) + matriz[1][1]*y(t))
+    sols = dsolve((eq1, eq2))
+    return sols[0].rhs, sols[1].rhs
